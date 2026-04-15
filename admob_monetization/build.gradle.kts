d1ea3c4a7a2d0ca190331a3976b5d8fc291d52bb
M_Imran <appsapiconsole@gmail.com>
2026-04-15 12:54:23 +0500

    0.0.5_meta

--- admob_monetization/build.gradle.kts
+++ admob_monetization/build.gradle.kts
@@ -43,7 +43,7 @@ android {
     }
 }
 group = "com.github.Muhammadimrankhan"
-version = "0.0.4_meta"
+version = "0.0.5_meta"
 
 
 afterEvaluate {
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Muhammadimrankhan"
                 artifactId = "Monetization_IkAdSdk"
-                version = "0.0.4_meta"
+                version = "0.0.5_meta"
             }
         }
     }
