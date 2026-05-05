c0fceda3bf0b9fc7fe34db83f74f19df1fae8686
M_Imran <appsapiconsole@gmail.com>
2026-05-05 17:10:45 +0500

    0.1.11_meta

--- admob_monetization/build.gradle.kts
+++ admob_monetization/build.gradle.kts
@@ -43,7 +43,7 @@ android {
     }
 }
 group = "com.github.Muhammadimrankhan"
-version = "0.1.11"
+version = "0.1.11_meta"
 
 
 afterEvaluate {
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Muhammadimrankhan"
                 artifactId = "Monetization_IkAdSdk"
-                version = "0.1.11"
+                version = "0.1.11_meta"
             }
         }
     }
@@ -74,6 +74,7 @@ dependencies {
 
     // Add the dependency for the Analytics library
     // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.ads.mediation:facebook:6.21.0.2")
     implementation("com.google.firebase:firebase-analytics")
 
 }
