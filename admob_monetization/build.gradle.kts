fa085c9a07f8a5d8ed243655a6050a43f9701781
M_Imran <appsapiconsole@gmail.com>
2026-08-10 16:04:26 +0500

    0.1.25_meta

--- admob_monetization/build.gradle.kts
+++ admob_monetization/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     }
 }
 group = "com.github.Muhammadimrankhan"
-version = "0.1.24_meta"
+version = "0.1.25_meta"
 
 
 afterEvaluate {
@@ -58,7 +58,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Muhammadimrankhan"
                 artifactId = "Monetization_IkAdSdk"
-                version = "0.1.24_meta"
+                version = "0.1.25_meta"
             }
         }
     }
@@ -78,7 +78,7 @@ dependencies {
     implementation("com.intuit.ssp:ssp-android:1.1.1")
     implementation("com.google.android.ump:user-messaging-platform:4.0.0")
     // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
 
     // Add the dependency for the Analytics library
     // When using the BoM, you don't specify versions in Firebase library dependencies
